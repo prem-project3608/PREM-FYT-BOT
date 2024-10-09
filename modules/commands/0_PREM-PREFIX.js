6313c035cc06ad47305a9af3744aad16273be9f4
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-09 22:25:31 +0530

    Create 0_PREM-PREFIX.js

---
+++ modules/commands/0_PREM-PREFIX.js
@@ -0,0 +1,38 @@
+module.exports.config = {
+  name: "prefix",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "PREM BABU",
+  description: "THIS BOT IS MADE BY PREM BABU",
+  commandCategory: "BOT-PREFIX",
+  usages: "PREFIX",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = async ({ event, api, Threads }) => {
+  var { threadID, messageID, body, senderID } = event;
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "PREM BABU") { return api.sendMessage(`PREM-BABU-BOT-CREATER 🙂`, threadID, messageID)}
+  function out(data) {
+    api.sendMessage(data, threadID, messageID)
+  }
+  var dataThread = (await Threads.getData(threadID));
+  var data = dataThread.data; 
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+  var arr = ["prefix","pref","prafix", "praf", "profix","प्रीफिक्स"];
+  arr.forEach(i => {
+    let str = i[0].toUpperCase() + i.slice(1);
+    if (body === i.toUpperCase() | body === i | str === body) {
+const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`❁ ══════ ❃[ ${prefix} ]❃ ══════ ❁`)
+      }
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️' + data.PREFIX)
+    }
+
+  });
+};
+
+module.exports.run = async({ event, api }) => {
+    return api.sendMessage("मेरी जान ये कमांड नो प्री फिक्स है। सिर्फ प्री फिक्स लिखो 🙂✌️", event.threadID)
+}
