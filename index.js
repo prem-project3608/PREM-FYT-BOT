4d010092afe8814df0082d70db372ae8c6269cd1
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-12 16:12:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ logger("Opened server site...", "[ Starting ]");
 function startBot(message) {
     (message) ? logger(message, "[ Starting ]") : "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "PREM-BABU.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "PREM-BABU-3.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
