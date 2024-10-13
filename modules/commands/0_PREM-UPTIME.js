1446a18493ec6c50179ad81c2e7e1f431fe2961a
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-14 03:13:39 +0530

    Update 0_PREM-UPTIME.js

--- modules/commands/0_PREM-UPTIME.js
+++ modules/commands/0_PREM-UPTIME.js
@@ -1,22 +1,17 @@
 const os = require('os');
 const uptime = os.uptime();
 
-// Delhi timezone ka current date aur time lene ke liye function
-function getDelhiTime() {
-  const options = { 
-    timeZone: 'Asia/Kolkata', 
-    hour12: true, // 12-hour format AM/PM ke saath
-    year: 'numeric', 
-    month: 'long',  // Poora month name
-    day: '2-digit', 
-    hour: '2-digit', 
-    minute: '2-digit', 
-    second: '2-digit', 
-    weekday: 'long'  // Din ka naam include karne ke liye
-  };
-  const formatter = new Intl.DateTimeFormat('en-IN', options);
-  const delhiTime = formatter.format(new Date());
-  return delhiTime;
+// India timezone ka current date aur time lene ke liye function
+function getIndiaTime() {
+  const currentDate = new Date();
+  const optionsTime = { hour12: true, timeZone: 'Asia/Kolkata' };
+  const optionsDate = { timeZone: 'Asia/Kolkata' };
+
+  const time = currentDate.toLocaleTimeString('en-IN', optionsTime);
+  const date = currentDate.toLocaleDateString('en-IN', optionsDate);
+  const day = currentDate.toLocaleString('en-IN', { weekday: 'long', timeZone: 'Asia/Kolkata' });
+
+  return { time, date, day };
 }
 
 module.exports.config = {
@@ -24,7 +19,7 @@ module.exports.config = {
   version: "1.0.1",
   hasPermssion: 0,
   credits: "PREM BABU",
-  description: "Displays bot uptime and current time in Delhi",
+  description: "Displays bot uptime and current time in India",
   commandCategory: "BOT RUNNING UPTIME",
   usages: "NO PREFIX",
   cooldowns: 5
@@ -39,14 +34,15 @@ module.exports.handleEvent = async ({ api, event }) => {
           hours = Math.floor(time / (60 * 60)),
           minutes = Math.floor((time % (60 * 60)) / 60),
           seconds = Math.floor(time % 60);
-    
-    const delhiTime = getDelhiTime();
-    
+
+    // India time aur date ko prapt karna
+    const { time: indiaTime, date: indiaDate, day: indiaDay } = getIndiaTime();
+
     // System uptime ko minutes mein convert karna
     const systemUptimeMinutes = Math.floor(uptime / 60);
     
     // Message body ko define karna
-    const randomMessage = `❁ ━━━━━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━━━━━ ❁\n\n\n☆ RUNNING TIME :- ${hours}H ${minutes}M ${seconds}S\n☆ SYSTEM UPDATE :- ${systemUptimeMinutes}\n☆ TIME OR DATE :- ${delhiTime}`;
+    const randomMessage = `❁ ━━━━━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━━━━━ ❁\n\n\n☆ RUNNING TIME :- ${hours}H ${minutes}M ${seconds}S\n☆ SYSTEM UPDATE :- ${systemUptimeMinutes}M\n☆ TIME :- ${indiaTime}\n☆ DATE :- ${indiaDate}\n☆ DAY :- ${indiaDay}`;
 
     // Message bhejna bina kisi attachment ke
     api.sendMessage({
