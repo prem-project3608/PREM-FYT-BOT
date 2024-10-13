7300278db5a6a4aadea2b9999de0778ed825f72c
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-13 22:27:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ app.get('/', function(req, res) {
 
 app.listen(port);
 
-logger("OPENED BOT SERVER SITE", "[ PREM BOT STARTING ]");
+logger("Opened server site...", "[ PREM PROJECT STARTING ]");
 
 /////////////////////////////////////////////////////////
 //========= Create start bot and make it loop =========//
 /////////////////////////////////////////////////////////
 
 function startBot(message) {
-    (message) ? logger(message, "[ PREM BOT STARTING ]") : "";
+    (message) ? logger(message, "[ PREM PROJECT STARTING ]") : "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "PREM-BABU-1.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "PREM-BABU.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
@@ -56,22 +56,38 @@ function startBot(message) {
     });
 
     child.on("error", function (error) {
-        logger("KUCH GADWAD HAI BHAYIA 😏" + JSON.stringify(error), "[ PREM BOT STARTING ]");
+        logger("An error occurred: " + JSON.stringify(error), "[ PREM PROJECT STARTING ]");
     });
 };
 ////////////////////////////////////////////////
-//========= MR PREM BABU BOT PROJECT =========//
+//========= Check update from Github =========//
 ////////////////////////////////////////////////
 
 
 axios.get("https://raw.githubusercontent.com/prem-project3608/PREM-FCA-BOT/refs/heads/main/PREM-FCA/package.json").then((res) => {
     logger(res['data']['name'], "[ NAME ]");
-    logger("Version: " + res['data']['version'], "[ PREM VERSION ]");
-    logger(res['data']['description'], "[ PREM DESCRIPTION ]");
+    logger("Version: " + res['data']['version'], "[ PREM PROJECT STARTING ]");
+    logger(res['data']['description'], "[ PREM PROJECT STARTING ]");
 });
 startBot();
-        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ PREM UPDATE ]'), 
+/*axios.get("https://raw.githubusercontent.com/d-jukie/miraiv2_fix/main/package.json").then((res) => {
+    const local = JSON.parse(readFileSync('./package.json'));
+    if (semver['lt'](local.version, res['data']['version'])) {
+        if (local.autoUpdate == !![]) {
+            logger('A new update is available, start update processing...', '[ UPDATE ]');
+            const updateBot = {};
+            updateBot.cwd = __dirname
+            updateBot.stdio = 'inherit' 
+            updateBot.shell = !![];
+            const child = spawn('node', ['update.js'], updateBot);
+            child.on('exit', function () {
+                return process.exit(0);
+            })
+            child.on('error', function (error) {
+                logger('Unable to update:' + JSON.stringify(error), '[ CHECK UPDATE ]');
+            });
+        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ UPDATE ]'), 
         startBot();
-    } else logger('You are using the latest version!', '[ PREM CHECK UPDATE ]'), startBot();
-}).catch(err => logger("Unable to check update.", "[ PREM CHECK UPDATE ]"));*/
+    } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
+}).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
 // THIS BOT WAS MADE BY MR PREM BABU DO NOT CHANGE CREDIT NAME
