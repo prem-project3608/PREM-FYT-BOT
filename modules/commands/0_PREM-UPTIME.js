59fffff6c9128927c6e695c3ababbebaede9240c
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-14 03:25:00 +0530

    Update 0_PREM-UPTIME.js

--- modules/commands/0_PREM-UPTIME.js
+++ modules/commands/0_PREM-UPTIME.js
@@ -15,7 +15,7 @@ function getIndiaTime() {
 }
 
 module.exports.config = {
-  name: "upt",
+  name: "upt", 
   version: "1.0.1",
   hasPermssion: 0,
   credits: "PREM BABU",
@@ -42,7 +42,7 @@ module.exports.handleEvent = async ({ api, event }) => {
     const systemUptimeMinutes = Math.floor(uptime / 60);
     
     // Message body ko define karna
-    const randomMessage = `❁ ━━━━━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━━━━━ ❁\n\n\n☆ RUNNING TIME :- ${hours}H ${minutes}M ${seconds}S\n☆ SYSTEM UPDATE :- ${systemUptimeMinutes}M\n☆ TIME :- ${indiaTime}\n☆ DATE :- ${indiaDate}\n☆ DAY :- ${indiaDay}`;
+    const randomMessage = `❁ ━━━━━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━━━━━ ❁\n\n\n✰ RUNNING TIME ➪ ${hours}H ${minutes}M ${seconds}S\n✰ SYSTEM UPDATE ➪ ${systemUptimeMinutes}M\n✰ TIME ➪ ${indiaTime}\n✰ DATE ➪ ${indiaDate}\n✰ DAY ➪ ${indiaDay}\n━━━━━━━━━━━━━━━━━━━━━\nMADE BY MR PREM BABU 🙂`;
 
     // Message bhejna bina kisi attachment ke
     api.sendMessage({
