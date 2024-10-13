e81d9296aece2d8570649c192dc0118cc4ba2ce1
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-13 23:03:31 +0530

    Update 0_PREM-UPTIME.js

--- modules/commands/0_PREM-UPTIME.js
+++ modules/commands/0_PREM-UPTIME.js
@@ -1,12 +1,20 @@
 const os = require('os');
 const uptime = os.uptime();
 
+// Function to get current date and time in Delhi timezone
+function getDelhiTime() {
+  const options = { timeZone: 'Asia/Kolkata', hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  const formatter = new Intl.DateTimeFormat([], options);
+  const delhiTime = formatter.format(new Date());
+  return delhiTime;
+}
+
 module.exports.config = {
   name: "upt",
   version: "1.0.1",
   hasPermssion: 0,
   credits: "PREM BABU",
-  description: "Displays bot uptime",
+  description: "Displays bot uptime and current time in Delhi",
   commandCategory: "BOT RUNNING UPTIME",
   usages: "NO PREFIX",
   cooldowns: 5
@@ -20,8 +28,10 @@ module.exports.handleEvent = async ({ api, event }) => {
           hours = Math.floor(time / (60 * 60)),
           minutes = Math.floor((time % (60 * 60)) / 60),
           seconds = Math.floor(time % 60);
+    
+    const delhiTime = getDelhiTime();
 
-    api.sendMessage({body:`Uptime: ${hours}h ${minutes}m ${seconds}s\nSystem Uptime: ${uptime} seconds`}, threadID, messageID);
+    api.sendMessage({body:`Uptime: ${hours}h ${minutes}m ${seconds}s\nSystem Uptime: ${uptime} seconds\nCurrent Time (Delhi): ${delhiTime}`}, threadID, messageID);
   }
 };
 
