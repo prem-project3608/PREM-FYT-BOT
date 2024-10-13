90613a9a13853f48324cf85191aae1968ed6de75
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-14 04:33:56 +0530

    Update 0_PREM-IMGBB.js

--- modules/commands/0_PREM-IMGBB.js
+++ modules/commands/0_PREM-IMGBB.js
@@ -2,21 +2,21 @@ module.exports.config = {
     name: "imgbb",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "PREM BABU",
-    description: "THIS BOT IS MADE BY MR PREM BABU",
-    commandCategory: "IMAGE LINK GENERATION",
-    usages: "REPLY TO IMAGE",
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    description: "",
+    commandCategory: "Game",
+    usages: "[reply]",
     cooldowns: 5,
     dependencies: {
-  "axios": "",}
+      "axios": ""
+    }
 };
- 
+
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if(!linkanh) return api.sendMessage('Please reply to image.', event.threadID, event.messageID)
-const res = await axios.get(`http://zekais-api.herokuapp.com/imgbb?url=${encodeURIComponent(linkanh)}&apikey=N3MqSq2j`);    
-var juswa = res.data.uploaded.image;
-    return api.sendMessage(`${juswa}`, event.threadID, event.messageID);
- 
+    if(!linkanh) return api.sendMessage('Please reply or enter a link 1 image!!!', event.threadID, event.messageID)
+const res = await axios.get(`https://imgur-api-by-koja.xx0xkoja.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
+var img = res.data.uploaded.image;
+    return api.sendMessage(`${img}`, event.threadID, event.messageID);
 }
