3995ffe14c07a7ae082ec255c6643174e8c1e613
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-13 22:30:10 +0530

    Update 0_PREM-STATUS-2.js

--- modules/commands/0_PREM-STATUS-2.js
+++ modules/commands/0_PREM-STATUS-2.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["-RAJA_STATUS-"];
+   var hi = ["-PREM_STATUS-"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
