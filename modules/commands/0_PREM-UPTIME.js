975401c6f7b8710c4ca7fe61b23cabdb70b5e20b
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-13 21:36:02 +0530

    Update 0_PREM-UPTIME.js

--- modules/commands/0_PREM-UPTIME.js
+++ modules/commands/0_PREM-UPTIME.js
@@ -1,43 +1,67 @@
+const fetch = require('node-fetch');
+const os = require('os');
+const maxCpu = os.cpus().length;
+const maxRam = os.totalmem();
+const maxRamInGB = (maxRam / 1024 / 1024 / 1024 / 30).toFixed(2);
+const fs = require('fs');
+const language = process.env.REPL_LANGUAGE;
+const platform = os.platform();
+const architecture = os.arch();
+const cpuModel = os.cpus()[0].model;
+const uptime = os.uptime();
+const nodejs = process.version;
+global.client.timeStart = new Date().getTime(),
+
 module.exports.config = {
-	name: "uptime",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "PREM BABU",
-	description: "THIS BOT WAS MADE BY MR PREM BABU",
-	commandCategory: "BOT RUNNING UPTIME",
-	usages: "NO PREFIX",
-	cooldowns: 5
+  name: "upt",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "R1zaX",
+  description: "no prefix",
+  commandCategory: "Không cần dấu lệnh",
+  usages: "xem thời gian bot onl",
+    cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, args, client }) {
-    const fs = require('fs-extra');
-    let time = process.uptime();
-	let hours = Math.floor(time / (60 * 60));
-	let minutes = Math.floor((time % (60 * 60)) / 60);
-	let seconds = Math.floor(time % 60);
-      const timeStart = Date.now();
-    var name = Date.now();
-    var url = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-    var lvbang = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-	  if(url.match(lvbang) == null) return api.sendMessage(`===「 𝗨𝗣𝗧𝗜𝗠𝗘 𝗥𝗢𝗕𝗢𝗧 」===\n\n𝗧𝗵𝗼̛̀𝗶 𝗴𝗶𝗮𝗻 𝗯𝗼𝘁 𝗵𝗶𝗲̣̂𝗻 𝗼𝗻𝗹𝗶𝗻𝗲 𝘁𝗼̂̉𝗻𝗴 𝗰𝗼̣̂𝗻𝗴 ${hours} 𝗴𝗶𝗼̛̀ ${minutes} 𝗽𝗵𝘂́𝘁 ${seconds} 𝗴𝗶𝗮̂𝘆 👾\n──────────────\nVui lòng nhập/reply url cần treo trên Uptime Robot!`, event.threadID, event.messageID);
-    var request = require("request");
-    var options = { method: 'POST',
-  url: 'https://api.uptimerobot.com/v2/newMonitor',
-  headers:
-   { 'content-type': 'application/x-www-form-urlencoded',
-     'noprefix-control': 'no-noprefix' },
-  form:
-   { api_key: 'u1521429-e69780eb556948775b151917',
-     format: 'json',
-     type: '1',
-     url: url,
-     friendly_name: name } };
-   /////////////////////////////////////////  //////Phần điều kiện và gửi tin nhắn//// ///////////////////////////////////////        
-request(options, function (error, response, body) {
-   if (error) return api.sendMessage(`Lỗi rồi huhu :((`, event.threadID, event.messageID );
-   if(JSON.parse(body).stat == 'fail') return api.sendMessage(`===「 𝗨𝗣𝗧𝗜𝗠𝗘 𝗥𝗢𝗕𝗢𝗧 」===\n\n𝗧𝗵𝗼̛̀𝗶 𝗴𝗶𝗮𝗻 𝗯𝗼𝘁 𝗵𝗶𝗲̣̂𝗻 𝗼𝗻𝗹𝗶𝗻𝗲 𝘁𝗼̂̉𝗻𝗴 𝗰𝗼̣̂𝗻𝗴 ${hours} 𝗴𝗶𝗼̛̀ ${minutes} 𝗽𝗵𝘂́𝘁 ${seconds} 𝗴𝗶𝗮̂𝘆 👾\n──────────────\n[ 𝗘𝗥𝗥𝗢𝗥 ] - 𝗦𝗲𝗿𝘃𝗲𝗿 𝗻𝗮̀𝘆 𝗵𝗶𝗲̣̂𝗻 𝘁𝗼̂̀𝗻 𝘁𝗮̣𝗶 𝘁𝗿𝗲̂𝗻 𝗨𝗽𝘁𝗶𝗺𝗲 𝗥𝗼𝗯𝗼𝘁 𝗿𝗼̂̀𝗶 🌸\n🔗 𝗟𝗶𝗻𝗸: ${url}`, event.threadID, event.messageID);
-  if(JSON.parse(body).stat == 'success')
- return
-api.sendMessage(`===「 𝗨𝗣𝗧𝗜𝗠𝗘 𝗥𝗢𝗕𝗢𝗧 」===\n\n𝗧𝗵𝗼̛̀𝗶 𝗴𝗶𝗮𝗻 𝗯𝗼𝘁 𝗵𝗶𝗲̣̂𝗻 𝗼𝗻𝗹𝗶𝗻𝗲 𝘁𝗼̂̉𝗻𝗴 𝗰𝗼̣̂𝗻𝗴 ${hours} 𝗴𝗶𝗼̛̀ ${minutes} 𝗽𝗵𝘂́𝘁 ${seconds} 𝗴𝗶𝗮̂𝘆 👾\n──────────────\n[ 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 ] - 𝗧𝗮̣𝗼 𝘀𝗲𝗿𝘃𝗲𝗿 𝘁𝗿𝗲̂𝗻 𝗨𝗽𝘁𝗶𝗺𝗲 𝗥𝗼𝗯𝗼𝘁 𝘁𝗵𝗮̀𝗻𝗵 𝗰𝗼̂𝗻𝗴 🌸\n🔗 𝗟𝗶𝗻𝗸: ${url}`,event.threadID, event.messageID );
-});
-                                    }
+function byte2mb(bytes) {
+	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	let l = 0, n = parseInt(bytes, 10) || 0;
+	while (n >= 1024 && ++l) n = n / 1024;
+	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
+}
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+const xuly = Math.floor((Date.now() - global.client.timeStart)/4444)
+var trinhtrang = xuly < 10 ? " 𝗧𝗼̂́𝘁 ✔️":
+  xuly > 10 && xuly < 100 ? "Ổn Định" : "Chậm";
+const pidusage = await global.nodemodule["pidusage"](process.pid);
+  if (!event.body) return;
+  var { threadID, messageID } = event;
+  const threadname = global.data.threadInfo.get(event.threadID).threadName || ((await Threads.getData(event.threadID)).threadInfo).threadName;
+  if (event.body.toLowerCase().indexOf("upt") == 0) {
+    const dateNow = Date.now();
+   const time = process.uptime(),
+	      gio = Math.floor(time / (60 * 60)),
+	      phut = Math.floor((time % (60 * 60)) / 60),
+	    	giay = Math.floor(time % 60);
+  const { commands } = global.client;
+  const { exec } = require('child_process');
+exec('du -sh', (error, stdout, stderr) => {
+  if (error) {
+    api.sendMessage(`Đã xảy ra lỗi: ${error.message}`, event.threadID, event.messageID);
+    return;
+  }
+  if (stderr) {
+    api.sendMessage(`Lỗi STDERR: ${stderr}`, event.threadID, event.messageID);
+    return;
+  }
+  
+  const storageUsed = stdout.trim();
+  const [size, path] = storageUsed.split('\t');
+
+    api.sendMessage({body:`━━━━[ 𝗨𝗣𝗧𝗜𝗠𝗘 ]━━━━\n\n            ${gio} : ${phut} : ${giay}\n\n 𝗡𝗴𝗼̂𝗻 𝗻𝗴𝘂̛̃: ${language}\n 𝗛𝗲̣̂ 𝗱𝗶𝗲̂̀𝘂 𝗵𝗮̀𝗻𝗵: ${platform} ${architecture}\n 𝗩-𝗡𝗼𝗱𝗲𝗝𝘀: ${nodejs}\n 𝗠𝗼𝗱𝗲𝗹 𝗖𝗣𝗨: ${cpuModel}\n 𝗠𝗲𝗺𝗼𝗿𝘆: ${size}B\n 𝗖𝗣𝗨: ${pidusage.cpu.toFixed(1)} % / ${maxCpu} 𝗖𝗣𝗨s\n 𝗥𝗔𝗠: ${byte2mb(pidusage.memory)} / ${maxRamInGB} GB\n 𝗣𝗶𝗻𝗴: ${Date.now() - dateNow} ms\n 𝗧𝗶̀𝗻𝗵 𝗧𝗿𝗮̣𝗻𝗴: ${trinhtrang}\n 𝗨𝗽𝘁𝗶𝗺𝗲 𝘀𝘆𝘀𝘁𝗲𝗺: ${uptime} giây`},event.threadID, event.messageID);
+      });
+   }
+};
+
+module.exports.run = () => {};
