03cef15448525c8a32cfd2b60bbf040e27b1b993
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-14 17:41:37 +0530

    Update 0_PREM-SONG-4.js

--- modules/commands/0_PREM-SONG-4.js
+++ modules/commands/0_PREM-SONG-4.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 
 module.exports.config = {
-  name: "gana",
+  name: "music",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "PREM BABU",
