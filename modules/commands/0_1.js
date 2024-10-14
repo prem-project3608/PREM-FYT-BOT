1573d249e844ff14355d2f71ec82ecb9eb044cb5
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-15 02:21:40 +0530

    Update 0_1.js

--- modules/commands/0_1.js
+++ modules/commands/0_1.js
@@ -1,36 +1,151 @@
 module.exports.config = {
-    name: "pin",
+    name: "video",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "D-Jukie",
-    description: "Tìm kiếm hình ảnh",
-    commandCategory: "game",
-    usages: "[Text]",
-    cooldowns: 0,
-};
-module.exports.run = async function({ api, event, args }) {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const request = require("request");
-    const keySearch = args.join(" ");
-    if(keySearch.includes("-") == false) return api.sendMessage('🌸Vui lòng nhập theo định dạng: từ khóa cần tìm kiếm - số ảnh cần tìm🌸', event.threadID, event.messageID)
-    const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
-    const numberSearch = keySearch.split("-").pop() || 6
-    const res = await axios.get(`https://0901.vinh-05.repl.co/pinterest?search=${encodeURIComponent(keySearchs)}`);
-    const data = res.data.data;
-    var num = 0;
-    var imgData = [];
-    for (var i = 0; i < parseInt(numberSearch); i++) {
-      let path = __dirname + `/cache/${num+=1}.jpg`;
-      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
-      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+    credits: "PREM BABU",
+    description: "THIS BOT IS MADE BY MR PREM BABU",
+    commandCategory: "VIDEO DANWLONDER",
+    usages: "PREFIX",
+    cooldowns: 10,
+    dependencies: {
+        "ytdl-core": "",
+        "simple-youtube-api": ""
     }
-    api.sendMessage({
-        attachment: imgData,
-        body: numberSearch + '🌸Kết quả tìm kiếm của từ khóa: '+ keySearchs
-    }, event.threadID, event.messageID)
-    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+}, module.exports.handleReply = async function({
+    api: e,
+    event: a,
+    handleReply: t
+}) {
+    const n = global.nodemodule.axios,
+        s = global.nodemodule["fs-extra"],
+        i = (global.nodemodule.request, await n.get("https://raw.githubusercontent.com/RqzaX040/Global-Hokai/main/video.json")),
+        r = i.data.keyVideo.length,
+        o = i.data.keyVideo[Math.floor(Math.random() * r)],
+        {
+            createReadStream: d,
+            createWriteStream: m,
+            unlinkSync: l,
+            statSync: h
+        } = global.nodemodule["fs-extra"];
+    var c, u = a.body;
+    if (c = u, isNaN(c) || (c < 1 || c > 6)) return e.sendMessage("1 से 6 तक के नंबर में सेलेक्ट कर दोस्त 🙂🤞", a.threadID, a.messageID);
+    e.unsendMessage(t.messageID);
+    try {
+        var g = {
+            method: "GET",
+            url: "https://ytstream-download-youtube-videos.p.rapidapi.com/dl",
+            params: {
+                id: `${t.link[a.body-1]}`
+            },
+            headers: {
+                "x-rapidapi-host": "ytstream-download-youtube-videos.p.rapidapi.com",
+                "x-rapidapi-key": `${o.API_KEY}`
+            }
+        };
+        var p = (await n.request(g)).data,
+            y = p.title;
+        if ("fail" == p.status) return e.sendMessage("ये फ़ाइल नहीं भेजी जा सकती 🤐🤞", a.threadID);
+        var f = Object.keys(p.link)[1],
+            b = p.link[f][0];
+        path1 = __dirname + "/cache/1.mp4";
+        const i = (await n.get(`${b}`, {
+            responseType: "arraybuffer"
+        })).data;
+        return s.writeFileSync(path1, Buffer.from(i, "utf-8")), e.unsendMessage(t.messageID), s.statSync(__dirname + "/cache/1.mp4").size > 96e6 ? e.sendMessage("Không thể gửi file vì dung lượng lớn hơn 25MB.", a.threadID, (() => l(__dirname + "/cache/1.mp4")), a.messageID) : e.sendMessage({
+            body: `${y}`,
+            attachment: s.createReadStream(__dirname + "/cache/1.mp4")
+        }, a.threadID, (() => s.unlinkSync(__dirname + "/cache/1.mp4")), a.messageID)
+    } catch {
+        return e.sendMessage("Không thể gửi file này!", a.threadID, a.messageID)
+    }
+    for (let e = 1; e < 7; e++) l(__dirname + `/cache/${e}.png`)
+}, module.exports.run = async function({
+    api: e,
+    event: a,
+    args: t
+}) {
+    const n = global.nodemodule.axios,
+        s = global.nodemodule["fs-extra"],
+        i = (global.nodemodule.request, await n.get("https://raw.githubusercontent.com/RqzaX040/Global-Hokai/main/video.json")),
+        r = i.data.keyVideo.length,
+        o = i.data.keyVideo[Math.floor(Math.random() * r)],
+        d = (global.nodemodule["ytdl-core"], global.nodemodule["simple-youtube-api"]),
+        {
+            createReadStream: m,
+            createWriteStream: l,
+            unlinkSync: h,
+            statSync: c
+        } = global.nodemodule["fs-extra"];
+    var u = ["AIzaSyD6NWUuQCNLRbdmjDTtXXunXP6vrYAgPsQ"];
+    const g = u[Math.floor(Math.random() * u.length)],
+        p = new d(g);
+    if (0 == t.length || !t) return e.sendMessage("वीडियो का नाम तो लिखो 😏🤞", a.threadID, a.messageID);
+    const y = t.join(" ");
+    if (0 == t.join(" ").indexOf("https://")) {
+        var f = {
+            method: "GET",
+            url: "https://ytstream-download-youtube-videos.p.rapidapi.com/dl",
+            params: {
+                id: t.join(" ").split(/^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/)[3]
+            },
+            headers: {
+                "x-rapidapi-host": "ytstream-download-youtube-videos.p.rapidapi.com",
+                "x-rapidapi-key": `${o.API_KEY}`
+            }
+        };
+        var b = (await n.request(f)).data,
+            v = b.title;
+        if ("fail" == b.status) return e.sendMessage("ये फ़ाइल नहीं भेजी जा सकती 🤐🤞", a.threadID);
+        var k = Object.keys(b.link)[1],
+            I = b.link[k][0];
+        path1 = __dirname + "/cache/1.mp4";
+        const i = (await n.get(`${I}`, {
+            responseType: "arraybuffer"
+        })).data;
+        return s.writeFileSync(path1, Buffer.from(i, "utf-8")), s.statSync(__dirname + "/cache/1.mp4").size > 96e6 ? e.sendMessage("ये वीडियो 25 MB से ज्यादा बड़ी है इसको नही भेजा जा सकता सॉरी 🤐🤞", a.threadID, (() => h(__dirname + "/cache/1.mp4")), a.messageID) : e.sendMessage({
+            body: `» ${v}`,
+            attachment: s.createReadStream(__dirname + "/cache/1.mp4")
+        }, a.threadID, (() => s.unlinkSync(__dirname + "/cache/1.mp4")), a.messageID)
+    }
+    try {
+        const t = global.nodemodule["fs-extra"],
+            n = global.nodemodule.axios;
+        var w = [],
+            _ = "",
+            D = 0,
+            S = 0,
+            M = [],
+            $ = await p.searchVideos(y, 6);
+        for (let e of $) {
+            if (void 0 === e.id) return;
+            w.push(e.id);
+            e.id;
+            let a = __dirname + `/cache/${S+=1}.png`,
+                s = `https://img.youtube.com/vi/${e.id}/hqdefault.jpg`,
+                i = (await n.get(`${s}`, {
+                    responseType: "arraybuffer"
+                })).data,
+                r = (await n.get(`https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=${e.id}&key=${g}`)).data.items[0].contentDetails.duration.slice(2).replace("S", "").replace("M", ":");
+            (await n.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${e.id}&key=${g}`)).data.items[0].snippet.channelTitle;
+            if (t.writeFileSync(a, Buffer.from(i, "utf-8")), M.push(t.createReadStream(__dirname + `/cache/${S}.png`)), 1 == (D = D += 1)) var x = "1";
+            if (2 == D) x = "2";
+            if (3 == D) x = "3";
+            if (4 == D) x = "4";
+            if (5 == D) x = "5";
+            if (6 == D) x = "6";
+            _ += `${x} • [ ${r} ] ${e.title}\n\n`
+        }
+        var j = `${w.length}\n\n${_}`;
+        return e.sendMessage({
+            attachment: M,
+            body: j
+        }, a.threadID, ((e, t) => global.client.handleReply.push({
+            name: this.config.name,
+            messageID: t.messageID,
+            author: a.senderID,
+            link: w
+        })), a.messageID)
+    } catch (t) {
+        return e.sendMessage("सॉरी दोस्त कुछ गड़वड़ है फिर से ट्राई कर 🤐🤞" + t.message, a.threadID, a.messageID)
     }
 };
