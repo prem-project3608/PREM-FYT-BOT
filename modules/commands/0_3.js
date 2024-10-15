004815c53db640926b1e1d10c93c7376df92afb4
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-15 14:16:14 +0530

    Update 0_3.js

--- modules/commands/0_3.js
+++ modules/commands/0_3.js
@@ -3,30 +3,34 @@ module.exports.config = {
     version: "2.0.2",
     hasPermssion: 2,
     credits: "Mirai Team mod by Jukie",
-    description: "Khởi động lai bot",
-    commandCategory: "Hệ thống admin-bot",
+    description: "Restart the bot",
+    commandCategory: "System admin-bot",
     usages: "restart",
     cooldowns: 5,
     dependencies: { }
 }
  
 module.exports.run = async function({ api, args, Users, event}) {
-const { threadID, messageID } = event;
-const axios = global.nodemodule["axios"];
+    const { threadID, messageID } = event;
+    const axios = global.nodemodule["axios"];
 
-const moment = require("moment-timezone");
-    var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH");
-    var phut = moment.tz("Asia/Ho_Chi_Minh").format("mm");
-    var giay = moment.tz("Asia/Ho_Chi_Minh").format("ss");
-const fs = require("fs");
-    let name = await Users.getNameUser(event.senderID)
-  if (event.senderID != 100070531069371) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
-if(args.length == 0) api.sendMessage(`💟Chào cậu chủ: ${name}\n🔰Cậu chủ vui lòng chờ trong giây lát, hệ thông bot sẽ khởi động lại sau 10s`,event.threadID, () =>process.exit(1))
-else{    
-let time = args.join(" ");
-setTimeout(() =>
-api.sendMessage(`🔮Bot sẽ khỏi động lại sau: ${gio}s\n⏰Bây giờ là: ${gio}:${phut}:${giay} `, threadID), 0)
-setTimeout(() =>
-api.sendMessage("⌛Đang bắt đầu quá trình khỏi động lại",event.threadID, () =>process.exit(1)), 1000*`${time}`);
-}
+    const moment = require("moment-timezone");
+    var hour = moment.tz("Asia/Ho_Chi_Minh").format("HH");
+    var minute = moment.tz("Asia/Ho_Chi_Minh").format("mm");
+    var second = moment.tz("Asia/Ho_Chi_Minh").format("ss");
+    const fs = require("fs");
+    
+    let name = await Users.getNameUser(event.senderID);
+    if (event.senderID != 100070531069371) 
+        return api.sendMessage(`[❗] Better luck next time!`, event.threadID, event.messageID);
+    
+    if (args.length == 0) 
+        api.sendMessage(`💟Hello boss: ${name}\n🔰Please wait a moment, the bot will restart in 10 seconds`, event.threadID, () => process.exit(1));
+    else {    
+        let time = args.join(" ");
+        setTimeout(() => 
+            api.sendMessage(`🔮The bot will restart in: ${hour}s\n⏰Current time: ${hour}:${minute}:${second} `, threadID), 0);
+        setTimeout(() => 
+            api.sendMessage("⌛Starting the restart process", event.threadID, () => process.exit(1)), 1000 * `${time}`);
+    }
 }
