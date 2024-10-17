44256ef5acf6e87ff92d3cd03f0a5ab2f770ab86
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 13:46:53 +0530

    Update 0_PREM-IMGUR.js

--- modules/commands/0_PREM-IMGUR.js
+++ modules/commands/0_PREM-IMGUR.js
@@ -1,3 +1,5 @@
+const API_KEY = "YOUR_API_KEY_HERE"; // यहाँ पर अपनी API कुंजी डालें
+
 module.exports.config = {
     name: "imgur",
     version: "1.0.0",
@@ -8,15 +10,29 @@ module.exports.config = {
     usages: "PREFIX",
     cooldowns: 1,
     dependencies: {
-  "axios": "",}
+        "axios": "",
+    }
 };
- 
+
 module.exports.run = async ({ api, event }) => {
-const axios = global.nodemodule['axios'];  
-var ZiaRein = event.messageReply.attachments[0].url || args.join(" ");
-    if(!ZiaRein) return api.sendMessage(`अरे यार फोटो को रिप्लाई कर के कमांड लिखो 🙂✌️`, event.threadID, event.messageID)
-const res = await axios.get(`https://api.phamvandien.xyz/imgur?link=${encodeURIComponent(ZiaRein)}`);    
-var ZiaReinn = res.data.uploaded.image;
-    return api.sendMessage(ZiaReinn, event.threadID, event.messageID);
- 
+    const axios = global.nodemodule['axios'];  
+    var ZiaRein = event.messageReply.attachments[0].url || args.join(" ");
+    
+    if (!ZiaRein) return api.sendMessage(`अरे यार फोटो को रिप्लाई कर के कमांड लिखो 🙂✌️`, event.threadID, event.messageID);
+    
+    try {
+        const res = await axios.post(`https://api.imgur.com/3/image`, {
+            image: ZiaRein // Imgur API को इमेज भेजें
+        }, {
+            headers: {
+                'Authorization': `Client-ID ${API_KEY}` // यहाँ API कुंजी का उपयोग करें
+            }
+        });
+
+        var ZiaReinn = res.data.data.link; // डाटा को सही तरीके से एक्सेस करें
+        return api.sendMessage(ZiaReinn, event.threadID, event.messageID);
+    } catch (error) {
+        console.error(error);
+        return api.sendMessage(`कुछ गलती हो गई, कृपया फिर से प्रयास करें।`, event.threadID, event.messageID);
+    }
 }
