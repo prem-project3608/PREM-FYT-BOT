750580f2fa2caa7cb86171902043fdba700dec23
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 10:00:46 +0530

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -2,10 +2,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.bold.hex("#FF0000").bold('[ ERROR ] » ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('PREM ERROR ❯') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FF0000").bold('[ ERROR ] »') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('PREM ERROR ❯') + data);
      break;
 		default:			        
       var job = ["#FFA500","#FFFF00","#00FF00","#0000FF", "#4B0082","#A020F0"];
