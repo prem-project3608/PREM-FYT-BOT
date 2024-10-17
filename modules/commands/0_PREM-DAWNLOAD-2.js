b0e7437d48c9c5582dce35de6bb63297936af86b
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 18:40:59 +0530

    Update 0_PREM-DAWNLOAD-2.js

--- modules/commands/0_PREM-DAWNLOAD-2.js
+++ modules/commands/0_PREM-DAWNLOAD-2.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
-const getFBInfo = require("@xaviabot/fb-downloader");
+const getFBInfo = require("prem-facebook");
 
 module.exports.config = {
   name: "PREM-VIDEO-DOWNLOAD",
@@ -16,55 +16,14 @@ module.exports.config = {
 
 module.exports.handleEvent = async function ({ api, event }) {
   if (event.body !== null && event.isGroup) {
-    const tiktokLinkRegex = /https:\/\/(www\.|vt\.)?tiktok\.com\//;
     const facebookLinkRegex = /https:\/\/www\.facebook\.com\/\S+/;
-    const capcutLinkRegex = /https:\/\/www\.capcut\.com\/t\/\S+/;
     const link = event.body;
-    
-    if (tiktokLinkRegex.test(link)) {
-      api.setMessageReaction("❤️", event.messageID, () => { }, true);
-      downloadAndSendTikTokContent(link, api, event);
-    } else if (facebookLinkRegex.test(link)) {
+
+    if (facebookLinkRegex.test(link)) {
       api.setMessageReaction("💙", event.messageID, () => { }, true);
       downloadAndSendFBContent(link, api, event);
-    } else if (capcutLinkRegex.test(link)) {
-      api.setMessageReaction("💛", event.messageID, () => { }, true);
-      downloadAndSendCapcutContent(link, api, event);
     }
   }
-};
-
-const downloadAndSendTikTokContent = async (url, api, event) => {
-  const regEx_tiktok = /https:\/\/(www\.|vt\.)?tiktok\.com\//;
-  const response = await axios.post(`https://www.tikwm.com/api/`, {
-    url: url
-  });
-  
-  const data = response.data.data;
-  const videoStream = await axios({
-    method: 'get',
-    url: data.play,
-    responseType: 'stream'
-  }).then(res => res.data);
-  
-  const fileName = `TikTok-${Date.now()}.mp4`;
-  const filePath = `./${fileName}`;
-  const videoFile = fs.createWriteStream(filePath);
-
-  videoStream.pipe(videoFile);
-
-  videoFile.on('finish', () => {
-    videoFile.close(() => {
-      console.log('DOWNLOAD TIKTOK VIDEO FILE');
-
-      api.sendMessage({
-        body: ``,
-        attachment: fs.createReadStream(filePath)
-      }, event.threadID, () => {
-        fs.unlinkSync(filePath);  
-      });
-    });
-  });
 };
 
 const downloadAndSendFBContent = async (url, api, event) => {
@@ -79,33 +38,6 @@ const downloadAndSendFBContent = async (url, api, event) => {
       attachment: fs.createReadStream(fbvid)
     }, event.threadID, () => {
       fs.unlinkSync(fbvid); 
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const downloadAndSendCapcutContent = async (url, api, event) => {
-  try {
-    const response = await axios.get(`https://jonellccapisprojectv2-a62001f39859.herokuapp.com/api/capcut?url=${url}`);
-    const { result } = response.data;
-
-    const capcutFileName = `Capcut-${Date.now()}.mp4`;
-    const capcutFilePath = `./${capcutFileName}`;
-
-    const videoResponse = await axios({
-      method: 'get',
-      url: result.video_ori,
-      responseType: 'arraybuffer'
-    });
-
-    fs.writeFileSync(capcutFilePath, Buffer.from(videoResponse.data, 'binary'));
-
-    api.sendMessage({
-      body: ``,
-      attachment: fs.createReadStream(capcutFilePath)
-    }, event.threadID, () => {
-      fs.unlinkSync(capcutFilePath);
     });
   } catch (e) {
     console.log(e);
