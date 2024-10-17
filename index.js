81f873797aef2c238374e28ac33bbdc6a00f7d47
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 09:04:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,30 +64,9 @@ function startBot(message) {
 ////////////////////////////////////////////////
 
 
-axios.get("https://raw.githubusercontent.com/prem-project3608/PREM-FCA-BOT/refs/heads/main/PREM-FCA/package.json").then((res) => {
+axios.get("https://raw.githubusercontent.com/prem-project3608/prem-babu-fca/refs/heads/main/PREM-FCA/package.json").then((res) => {
     logger(res['data']['name'], "[ NAME ]");
     logger("Version: " + res['data']['version'], "[ PREM PROJECT STARTING ]");
     logger(res['data']['description'], "[ PREM PROJECT STARTING ]");
 });
 startBot();
-/*axios.get("https://raw.githubusercontent.com/d-jukie/miraiv2_fix/main/package.json").then((res) => {
-    const local = JSON.parse(readFileSync('./package.json'));
-    if (semver['lt'](local.version, res['data']['version'])) {
-        if (local.autoUpdate == !![]) {
-            logger('A new update is available, start update processing...', '[ UPDATE ]');
-            const updateBot = {};
-            updateBot.cwd = __dirname
-            updateBot.stdio = 'inherit' 
-            updateBot.shell = !![];
-            const child = spawn('node', ['update.js'], updateBot);
-            child.on('exit', function () {
-                return process.exit(0);
-            })
-            child.on('error', function (error) {
-                logger('Unable to update:' + JSON.stringify(error), '[ CHECK UPDATE ]');
-            });
-        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ UPDATE ]'), 
-        startBot();
-    } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
-}).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
-// THIS BOT WAS MADE BY MR PREM BABU DO NOT CHANGE CREDIT NAME
