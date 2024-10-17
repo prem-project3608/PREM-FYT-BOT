cf8677e7941d1a79e13844d7414ef584eb2a8952
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 19:47:08 +0530

    Create 0_1.js

---
+++ modules/commands/0_1.js
@@ -0,0 +1,46 @@
+let axios = require('axios');
+let fs = require('fs');
+
+let is_url = url => /^http(s|):\/\//.test(url);
+let stream_url = (url, type) => axios.get(url, {
+  responseType: 'arraybuffer'
+}).then(res => {
+  let path = __dirname + '/cache/' + Date.now() + '.' + type;
+
+  fs.writeFileSync(path, res.data);
+  setTimeout(p => fs.unlinkSync(p), 1000 * 60, path);
+
+  return fs.createReadStream(path);
+});
+
+exports.config = {
+  name: 'AUTOMETIC-3',
+  version: '0.0.1',
+  hasPermssion: 4,
+  credits: 'PREM BABU',
+  description: 'THIS BOT IS MADE BY MR PREM BABU',
+  commandCategory: 'INSTAGRAM VIDEO DAWNLODER',
+  usages: 'AUTOMETIC',
+  cooldowns: 3
+};
+
+exports.run = function(o) {};
+
+exports.handleEvent = async function(o) {
+  try {
+    let a = o.event.args[0];
+    let send = (msg, callback) => o.api.sendMessage(msg, o.event.threadID, callback, o.event.messageID);
+    let head = app => `━━━〈 𝗔𝗨𝗧𝗢 𝗗𝗢𝗪𝗡 ${app.toUpperCase()} 〉━━━\n\n`;
+
+    if (!is_url(a)) return;
+    
+    // Instagram content downloading logic
+    if (/instagram\.com\/(reel|p)\/\w+/.test(a)) {
+      let res = await axios.get(`https://www.nguyenmanh.name.vn/api/igDL?url=${a}&apikey=FI6bX3kC`);
+      send({
+        body: `${head('𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠')}➜ Tiêu đề: ${res.data.result.title}`,
+        attachment: await stream_url(res.data.result.video[0].url, 'mp4')
+      });
+    }
+  } catch {};
+};
