15f1031db490674d2d4883b182c4f296960018fa
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-20 15:26:46 +0530

    Update 0_PREM-IMGUR.js

--- modules/commands/0_PREM-IMGUR.js
+++ modules/commands/0_PREM-IMGUR.js
@@ -14,36 +14,42 @@ module.exports.run = async ({ api, event }) => {
     const qs = require('querystring');
 
     // Replace these with your actual client ID and client secret from Imgur
-    const client_id = 'b4808c95bb518c1';
-    const client_secret = '915e1e39335befeffa7a04e742eae6f452380f4c';
+    const client_id = 'b4808c95bb518c1'; // Your client ID
+    const client_secret = '915e1e39335befeffa7a04e742eae6f452380f4c'; // Your client secret
 
-    // Token generation request with correct format
-    const tokenResponse = await axios.post('https://api.imgur.com/oauth2/token', qs.stringify({
-        client_id: client_id,
-        client_secret: client_secret,
-        grant_type: 'client_credentials'
-    }), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+    try {
+        // Token generation request with correct format
+        const tokenResponse = await axios.post('https://api.imgur.com/oauth2/token', qs.stringify({
+            client_id: client_id,
+            client_secret: client_secret,
+            grant_type: 'client_credentials'
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const accessToken = tokenResponse.data.access_token;
+
+        const link = event.messageReply.attachments[0]?.url; // Ensure link exists
+        if (!link) {
+            return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
         }
-    });
 
-    const accessToken = tokenResponse.data.access_token;
+        // Upload image using the access token with correct URL
+        const imageUploadResponse = await axios.post('https://api.imgur.com/3/image', {
+            image: link
+        }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
 
-    var link = event.messageReply.attachments[0].url;
-    if (!link) {
-        return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
+        const result = imageUploadResponse.data.data.link;
+        return api.sendMessage(`Image uploaded successfully! Here is your link: ${result}`, event.threadID, event.messageID);
+
+    } catch (error) {
+        console.error(error);
+        return api.sendMessage('There was an error uploading the image: ' + error.message, event.threadID, event.messageID);
     }
-
-    // Upload image using the access token with correct URL
-    const imageUploadResponse = await axios.post('https://api.imgur.com/3/image', {
-        image: link
-    }, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    var result = imageUploadResponse.data.data.link;
-    return api.sendMessage(result, event.threadID, event.messageID);
 };
