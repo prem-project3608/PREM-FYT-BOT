b016d66e2d0910bc01797602a61e5f83bfcabe15
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-20 16:30:45 +0530

    Update 0_1.js

--- modules/commands/0_1.js
+++ modules/commands/0_1.js
@@ -3,24 +3,24 @@ module.exports.config = {
     version: "1.0.5",
     hasPermssion: 0,
     credits: "PREM BABU",
-    description: "THIS BOT WAS MADE BYR PREM BABU",
+    description: "THIS BOT WAS MADE BY PREM BABU",
     commandCategory: "ALL UID",
     cooldowns: 2,
 };
+
 module.exports.run = async function ({ api, event, args, Users }) {
   
-  function reply(d) {
-    api.sendMessage(d, event.threadID, event.messageID)
-  }
-  var ep = event.participantIDs;
-  msg = ""
-  msgs = ""
-  m = 0;
-  for (let i of ep) {
-    m += 1;
-    const name = await Users.getNameUser(i);
-    msg += m+". "+name+"\nUID: "+i+"\n\n";
-  }
-  msgs += "List of all UID's in this group.\n\n"+msg;
-  reply(msgs)
-}
+    function reply(d) {
+        api.sendMessage(d, event.threadID, event.messageID);
+    }
+  
+    var ep = event.participantIDs;
+    let msg = "List of all members' UID in this group:\n\n"; // मुख्य शीर्षक
+
+    for (let m = 0; m < ep.length; m++) {
+        const name = await Users.getNameUser(ep[m]);
+        msg += `${m + 1}. ${name} [ ${ep[m]} ]\n____________________________\n`; // नया फॉर्मेट
+    }
+
+    reply(msg);
+};
