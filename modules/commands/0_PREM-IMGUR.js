f2f66d4c7ae1fd414042b267eb2422163d50cb1a
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-20 15:48:35 +0530

    Update 0_PREM-IMGUR.js

--- modules/commands/0_PREM-IMGUR.js
+++ modules/commands/0_PREM-IMGUR.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const qs = require('querystring');
+
 module.exports.config = {
     name: "imgur",
     version: "1.0.0",
-    hasPermssion: 0,
+    hasPermission: 0,
     credits: "Deku (Modified by you)",
     description: "imgur upload using OAuth2",
     commandCategory: "imgur",
@@ -10,15 +15,12 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event }) => {
-    const axios = require('axios');
-    const qs = require('querystring');
-
     // Replace these with your actual client ID and client secret from Imgur
     const client_id = 'b4808c95bb518c1'; // Your client ID
     const client_secret = '915e1e39335befeffa7a04e742eae6f452380f4c'; // Your client secret
 
     try {
-        // Token generation request with correct format
+        // Token generation request
         const tokenResponse = await axios.post('https://api.imgur.com/oauth2/token', qs.stringify({
             client_id: client_id,
             client_secret: client_secret,
@@ -36,9 +38,27 @@ module.exports.run = async ({ api, event }) => {
             return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
         }
 
-        // Upload image using the access token with correct URL
+        // Download the image to a local file
+        const imagePath = path.join(__dirname, 'downloaded_image.png'); // Specify the path where you want to save the image
+        const response = await axios({
+            url: link,
+            method: 'GET',
+            responseType: 'stream'
+        });
+        
+        // Pipe the image data to the file
+        const writer = fs.createWriteStream(imagePath);
+        response.data.pipe(writer);
+
+        // Wait until the file has been written
+        await new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
+
+        // Upload the image using the access token
         const imageUploadResponse = await axios.post('https://api.imgur.com/3/image', {
-            image: link
+            image: fs.readFileSync(imagePath).toString('base64') // Read the image file and convert it to base64
         }, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
@@ -46,10 +66,14 @@ module.exports.run = async ({ api, event }) => {
         });
 
         const result = imageUploadResponse.data.data.link;
+
+        // Clean up the downloaded image file
+        fs.unlinkSync(imagePath); // Delete the local file after upload
+
         return api.sendMessage(`Image uploaded successfully! Here is your link: ${result}`, event.threadID, event.messageID);
 
     } catch (error) {
-        console.error(error);
-        return api.sendMessage('There was an error uploading the image: ' + error.message, event.threadID, event.messageID);
+        console.error('Error details:', error.response ? error.response.data : error.message);
+        return api.sendMessage('There was an error uploading the image: ' + (error.response ? error.response.data.message : error.message), event.threadID, event.messageID);
     }
 };
