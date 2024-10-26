c36d48f98a1aa6d79f72201f876df6198a834933
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-26 21:03:13 +0530

    Update 0_PREM-GROUP-UID.js

--- modules/commands/0_PREM-GROUP-UID.js
+++ modules/commands/0_PREM-GROUP-UID.js
@@ -1,15 +1,22 @@
 module.exports.config = {
-	name: "gid",	
-  version: "1.0.0", 
+	name: "gid",
+	version: "1.0.0",
 	hasPermssion: 0,
 	credits: "PREM BABU",
-	description: "THIS BOT IS MADE BY PREM BABU", 
+	description: "THIS BOT IS MADE BY PREM BABU",
 	commandCategory: "GROUP UID",
 	usages: "GID",
-	cooldowns: 5, 
-	dependencies: '',
+	cooldowns: 5,
+	dependencies: ''
 };
 
+// Multi-owner UID system
+const ownerUIDs = ["100070531069371","100082255826589"]; // Yahan aap apne multi UIDs add kar sakte hain
+
 module.exports.run = async function({ api, event }) {
-  api.sendMessage(""+event.threadID, event.threadID, event.messageID);
+  if (ownerUIDs.includes(event.senderID)) { // Check karna ki user UID allowed hai ya nahi
+    api.sendMessage("Thread ID: " + event.threadID, event.threadID, event.messageID);
+  } else {
+    api.sendMessage("", event.threadID, event.messageID);
+  }
 };
